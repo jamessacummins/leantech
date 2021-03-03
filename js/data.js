c6b26cdc6b4da0ff3476b710916ce263336d8b47
James Cummins <jamessacummins@gmail.com>
2021-03-04 09:54:24 +1100

    updating gifs

--- js/data.js
+++ js/data.js
@@ -95,18 +95,18 @@ const wiki_data = [
 	        		text:`If you are not logged in click the forgot password button, 
                             then enter your email address and click the send button. 
                              Then check your inbox, and follow the prompts in the email your receive.`,
-                    gif:`gif/forgot_3.gif`
+                    gif:`gif/forgot_3.gif`,
 	       
         		},
         		{
 	        		text:`If you are logged in, first navigate to the Account tab.`,
-                    gif:`gif/site_1.gif`
+                    gif:`gif/site_1.gif`,
 	        
         		},
         		{
 	        		text:`Then click the Change Password button and enter your current password and your desired password. 
                     Click Save to save your changes.`,
-                    gif:`gif/forgot_1.gif`,
+                    gif:`gif/password_3.gif`,
 	        
         		},
         ],
@@ -125,6 +125,7 @@ const wiki_data = [
         		{
 	        		text:`Then click the edit profile button, and enter any changes you wish to make to your account. 
                     You can change your name, email, company or project here.`,
+                    gif:`gif/profile_edit.gif`,
         		},
         		
         ],
@@ -139,23 +140,20 @@ const wiki_data = [
                     That is it is a way of organising your equipment items already in the app. 
                     Start by logging into the app, then navigate to the List tab. 
                     Then click the green plus button to add a list.`,
+                    gif:`gif/list_1.gif`,
                 },
                 {
                     text:`Enter the name you wish to call the list and hit the Create button.`,
+                    gif:`gif/list_2.gif`,
                 },
                 {
                     text:`Your list has now been created and you can 
                     start adding items that you’ve already created. 
                     Navigate to the Items tab and press and hold the first item 
-                    you wish to add until the app switches to select mode.`,
-                },
-                {
-                    text:`Once the first item has been selected and the app is in select mode, 
+                    you wish to add until the app switches to select mode. Once the first item has been selected and the app is in select mode, 
                     you can now bulk select the remaining desired items by tapping each. 
-                    Select all the items you wish to add.`,
-                },
-                {
-                    text:`Then, hit the add list button and select the list you wish your selected items to be added to.`,
+                    Select all the items you wish to add. Then, hit the add list button and select the list you wish your selected items to be added to.`,
+                    gif:'gif/list_3.gif',
                 },
         ],
     },
@@ -167,15 +165,14 @@ const wiki_data = [
                 {
                     text:`You can sort a list to help you quickly find items within a list you’ve already made. 
                     Start by logging in and navigating to the List tab.`,
+                    gif:`gif/list_1.gif`,
                 },
                 {
                     text:`Click the list you wish to sort to access it, 
-                    then click then AZ filter button to see possible sorts.`,
-                },
-                {
-                    text:`You can click the Title button to sort items alphabetically, 
+                    then click then AZ filter button to see possible sorts. You can click the Title button to sort items alphabetically, 
                     click the Hire date to sort items by date of hire, 
                     and click Status to sort items by where they are in the hire lifecycle.`,
+                    gif:`gif/filter_2.gif`,
                 },
         ],
     },
@@ -199,24 +196,26 @@ const wiki_data = [
                     available to be hired currently. You can view these items of equipment and easily add 
                     items from the market catalogue to your managed items.
                     Start by logging in and navigating to the Market tab.`,
+                    gif:`gif/market_1.gif`,
                 },
                 {
                     text:`Then, navigate to specific categories of items by selecting the category(s) of
                     the item you wish to browse.`,
+                    gif:`gif/market_2.gif`,
                 },
                 {
                     text:`Once you are in the correct category, you can browse items available for hire.
                     You can sort these by pressing the AZ filter button then 
                     sorting alphabetically, by price, or by delivery fee.`,
+                    gif:`gif/market_3.gif`,
                 },
                 {
-                    text:`Once you've found the desired item, click see more to view its details.`,
+                    text:`Once you've found the desired item, click see more to view its details.Then to add it to your saved items, click the save item button, and then click the copy button.`,
+                    gif:`gif/market_4.gif`,
                 },
                 {
-                    text:`To add it to your saved items, click the save item button, and then click the copy button.`,
-                },
-                {
-                    text:`Add changes as necessary then click the Save button to add the new item to your saved items..`,
+                    text:`Add changes as necessary then click the Save button to add the new item to your saved items.`,
+                    gif:`gif/market_5.gif`,
                 },
 
         ],
@@ -228,17 +227,18 @@ const wiki_data = [
         step:[
                 {
                     text:`Start by logging into the app, and navigating to the Items tab to see your current items.`,
+                    gif:`gif/send_1.gif`,
                 },
                 {
                     text:`Tap the item you want to hire and then tap the email item under the Supplier email field.`,
+                    gif:`gif/send_2.gif`,
                 },
                 {
-                    text:`Pick an email template to be generated, and the app will open your default email app.
-                     The email will automatically be filled with the correct fields ready to go.`,
+                    text:`Pick an email template to be generated, and the app will open your default email app. The email will automatically be filled with the correct fields ready to go.`,
+                    gif:`gif/send_3.gif`,
                 },
                 {
-                    text:`Edit as much as you wish, or not at all, then hit send to execute the request.
-                    Congratulations on saving time and energy and getting your hire request out!`,
+                    text:`Edit as much as you wish, or not at all, then hit send to execute the request. Congratulations on saving time and energy and getting your hire request out!`,
                 },
         ],
     },
@@ -250,24 +250,21 @@ const wiki_data = [
                 {
                     text:`Email templates are a way of easily automating the process of communicating with suppliers.
                     To start making or editing an email, log into the app, then navigate to the Account tab.`,
+                    gif:`gif/site_1.gif`,
                 },
                 {
                     text:`Click the Email templates button then tap the green plus button to add an email template.`,
+                    gif:`gif/template_2.gif`,
                 },
                 {
-                    text:`The Template Name is what the app will display to you. 
-                    The Subject is the subject of the email, and the Body the body of the email. 
-                    Enter your desired message then add custom fields if you wish.`,
+                    text:`The Template Name is what the app will display to you. The Subject is the subject of the email, and the Body the body of the email. Enter your desired message then add custom fields if you wish.`,
+                    gif:`gif/template_3.gif`,
                 },
                  {
-                    text:`You can see custom fields below in the options tab. 
-                    To insert a custom field type the option as it appears exactly, 
-                    making sure to add two curly braces on both sides. 
-                    When you use this template, each custom field will automatically be 
-                    filled in for you based on the item and conditions. 
-                    For example, 'Could I please pick up at the {{branch}} branch?' will automatically
-                     turn into 'Could I please pick up at the Southmore branch?' if the 
-                     item is attached to the Southmore branch.`,
+                    text:`You can see custom fields below in the options tab. To insert a custom field type the option as it appears exactly, making sure to add two curly braces on both sides. When you use this template, each custom field will automatically be filled in for you based on the item and conditions. 
+                    For example, 'Could I please pick up at the {{branch}} branch?' will automatically turn into 'Could I please pick up at the Southmore branch?' if the 
+                    item is attached to the Southmore branch.`,
+                    gif:`gif/template_4.gif`,
                 },
         ],
     },
